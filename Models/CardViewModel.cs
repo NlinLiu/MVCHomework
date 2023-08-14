bb82beec74bd007e1119bb8c9221f3968c98cd85
Nlin <smile72386@pic.net.tw>
2023-08-14 08:49:52 +0800

    將卡片改寫Partail view

---
+++ Models/CardViewModel.cs
@@ -0,0 +1,7 @@
+public class CardViewModel
+{
+    public string ImageUrl { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public List<string> Links { get; set; }
+}
